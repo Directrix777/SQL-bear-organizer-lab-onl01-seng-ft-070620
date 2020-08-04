402e675ea00aeb3454ee894ff27ef8d4afc5658e
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-08-04 18:15:43 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "Male", "Brown", "Nice", 1);
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "Male", "Blue", "Rowdy(surprise!)", 1);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "Female", "Lime Green", "Nice", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Tabitha", 2, "Female", "Lime Green", "Nice", 1);
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 2, "Female", "Brown", "Strict", 1);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Melissa", 13, "Female", "Pink", "Nice", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Melissa", 2, "Female", "Pink", "Nice", 1);
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Grinch", 2, "Male", "Dark Green", "Mean one", 1);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Wendy", 6, "Female", "Lavender", "Nice", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Wendy", 2, "Female", "Lavender", "Nice", 1);
 INSERT INTO bears(age, gender, color, temperament, alive) VALUES (2, "Other", "Purple", "Murderous", 1);
