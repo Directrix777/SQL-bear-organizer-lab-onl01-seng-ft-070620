8bdfb51ae18f33863725df7300fe06d4b7fb5203
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-08-04 17:56:49 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "Male", "Brown", "Nice", 1);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ();
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ();
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ();
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ();
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ();
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ();
+-- INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ();
+-- INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ();
+-- INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ();
+-- INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ();
+-- INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ();
+-- INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ();
 INSERT INTO bears(age, gender, color, temperament, alive) VALUES (2, "Other", "Purple", "Murderous", 1);
